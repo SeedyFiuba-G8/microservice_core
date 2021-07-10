b2b80959f144a6324f8747f9a8fdc81c91c1b0d1
Mauro Parafati <mparafati@fi.uba.ar>
2021-07-09 21:48:24 -0300

    [MOD] Scripts

--- init_db.sh
+++ init_db.sh
@@ -6,9 +6,6 @@ DEFAULT_URL='postgres://postgres:postgres@localhost:5432/sf_core'
 : "${DATABASE_URL:=$DEFAULT_URL}"
 
 cd ./scripts
-echo "Seedy FIUBA - Core microservice\n"
-
-echo "> CREATING TABLES AND INSERTING EXAMPLE DATA..."
 psql $DATABASE_URL -f init_db.sql
 
 echo "\n> Done! :)"
--- remote_dbs.sh
+++ remote_dbs.sh
@@ -3,8 +3,11 @@
 # This script will automatically update remote dbs
 # DANGER: This sould be removed when repository is made public!
 
-PROD_URL='postgres://nwzilirwqwlaya:633be5d76d509372e99c7430f5c316ffad1fb393630a5c6c7a4fc6f41ecb4fbf@ec2-3-212-75-25.compute-1.amazonaws.com:5432/d8i9eai6f8jjfe'
 DEV_URL='postgres://ujlehfjywtfqvb:2001d745df5e3e1b7474df770ea604bc4009a9f7ae83d80d75b36dc502133b62@ec2-18-233-83-165.compute-1.amazonaws.com:5432/dcqh2n319p9suc'
+PROD_URL='postgres://nwzilirwqwlaya:633be5d76d509372e99c7430f5c316ffad1fb393630a5c6c7a4fc6f41ecb4fbf@ec2-3-212-75-25.compute-1.amazonaws.com:5432/d8i9eai6f8jjfe'
 
-DATABASE_URL=$PROD_URL ./init_db.sh
+echo "\n> Updating dev..."
 DATABASE_URL=$DEV_URL ./init_db.sh
+
+echo "\n> Updating prod..."
+DATABASE_URL=$PROD_URL ./init_db.sh
